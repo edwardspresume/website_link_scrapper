5ed6e6a84bb25ecd93853c71e806727b29299da6
edwardspresume <edwardspresume@gmail.com>
2023-02-22 16:49:08 -0500

    made link display more readable

--- src/components/Details.svelte
+++ src/components/Details.svelte
@@ -3,12 +3,12 @@
     export let urls;
 </script>
 
-<details class="mb-2">
+<details class="mb-4">
     <summary>{title}</summary>
 
     <ul class="mt-2">
         {#each urls as url}
-            <li>{url}</li>
+            <li class="mt-2 odd:bg-gray-200 p-1">{url}</li>
         {/each}
     </ul>
 </details>
