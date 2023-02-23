5baa0ca8ba10bad09f7c19cbea54498cf7db0d05
edwardspresume <edwardspresume@gmail.com>
2023-02-23 09:54:07 -0500

    enter key hits for virtual keyboard

--- src/components/UrlInput.svelte
+++ src/components/UrlInput.svelte
@@ -32,6 +32,8 @@
 <input
     id="urlInput"
     type="search"
+    inputmode="url"
+    enterkeyhint="send"
     placeholder="Enter the url for the page you want to search"
     class="w-full p-2 pl-3 bg-black shadow placeholder:text-slate-400 rounded-xl"
     bind:value={url}
